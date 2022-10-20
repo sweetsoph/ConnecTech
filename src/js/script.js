368e73b9face16229c34ec37128f9f4353fb16d6
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-10-20 18:42:53 -0300

    telas de cadastro cliente e cadastro psicologo

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const buttonCadastrar = document.querySelector('#button-cadastrar');
+getElementById('button-cadastrar').addEventListener('click', function() {
+    console
+});
+buttonCadastrar.addEventListener('click', function () {
+    window.location.href = '../../screens/cadastro.html';
+});
